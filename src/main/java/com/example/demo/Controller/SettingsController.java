7a9d3fea9fba52a43a4f352c304bb2e3a4cb2326
xenonial98 <mirza898@gmail.com>
2022-12-15 01:55:10 +0800

    Create a return controller

--- src/main/java/com/example/demo/Controller/SettingsController.java
+++ src/main/java/com/example/demo/Controller/SettingsController.java
@@ -2,14 +2,12 @@ package com.example.demo.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class SettingsController {
+public class SettingsController extends ReturningController {
 
     @FXML
     private Button backBtn;
@@ -19,16 +17,11 @@ public class SettingsController {
 
     @FXML
     private Text settingsTextTitle;
-    private static Scene prevScene;
 
     @FXML
     void returnMenu(ActionEvent event) {
         Stage primStage = (Stage) this.backBtn.getScene().getWindow();
-        primStage.setScene(prevScene);
+        primStage.setScene(this.getPrevScene());
         primStage.show();
-    }
-
-    public static void setPrevScene(Scene menu) {
-        prevScene = menu;
     }
 }
